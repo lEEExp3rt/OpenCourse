c952368b7f5ca6549d1867ad312f318c6b8fe514
1EEExp3rt <1EEExpErt@zju.edu.cn>
2025-05-18 13:14:39 +0000

    feat: 实现用户历史操作、资源实体的模型层定义
    基于历史操作提取出一个公共基类用于后续开发，并构建了所有实体模型层的基本框架

---
+++ src/main/java/org/opencourse/models/Interaction.java
@@ -0,0 +1,14 @@
+package org.opencourse.models;
+
+/**
+ * Interaction entity class to represent a comment in interaction system in OpenCourse.
+ * 
+ * @author !EEExp3rt
+ */
+public class Interaction extends ActionObject {
+    
+    @Override
+    public Integer getId() {
+        return null; // TODO
+    }
+}
