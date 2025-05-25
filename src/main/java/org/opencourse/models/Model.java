6734683c216b8a46472dbda572833db9cf7c3690
1EEExp3rt <1EEExpErt@zju.edu.cn>
2025-05-25 08:52:49 +0000

    style: 添加 Model 基类部分注释

--- src/main/java/org/opencourse/models/Model.java
+++ src/main/java/org/opencourse/models/Model.java
@@ -3,8 +3,10 @@ package org.opencourse.models;
 /**
  * Abstract base class for all models in OpenCourse.
  * 
+ * @param <T> The type of the ID of the model, which extends Number.
  * @author !EEExp3rt
  * @apiNote This class serves as a base for all models in the OpenCourse application.
+ * @implNote The class is abstract and cannot be instantiated directly.
  */
 public abstract class Model<T extends Number> {
 
