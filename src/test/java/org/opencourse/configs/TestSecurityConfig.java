ca165ea4e41929a3730ee93b000162c8da1c49b8
JingXiong <jingxionglee337@gmail.com>
2025-05-31 22:36:10 +0800

    fix: repair some JWT bean injection in ControllerTest.

--- src/test/java/org/opencourse/configs/TestSecurityConfig.java
+++ src/test/java/org/opencourse/configs/TestSecurityConfig.java
@@ -1,5 +1,7 @@
 package org.opencourse.configs;
 
+import org.opencourse.repositories.UserRepo;
+import org.opencourse.utils.JwtUtils;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
@@ -7,6 +9,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.mockito.Mockito.mock;
 
 /**
  * Test security configuration to disable security for unit tests.
@@ -16,6 +21,23 @@ import org.springframework.security.web.SecurityFilterChain;
 @TestConfiguration
 @EnableWebSecurity
 public class TestSecurityConfig {
+
+    @Bean
+    @Primary
+    public JwtUtils jwtUtils() {
+        JwtUtils jwtUtils = new JwtUtils();
+        // Set test values using reflection to avoid property placeholder issues
+        ReflectionTestUtils.setField(jwtUtils, "secret",
+                "test-jwt-secret-key-for-unit-tests-should-be-at-least-32-characters");
+        ReflectionTestUtils.setField(jwtUtils, "expirationTime", 86400000L);
+        return jwtUtils;
+    }
+
+    @Bean
+    @Primary
+    public UserRepo userRepo() {
+        return mock(UserRepo.class);
+    }
 
     @Bean
     @Primary
--- src/test/java/org/opencourse/controllers/DepartmentControllerTest.java
+++ src/test/java/org/opencourse/controllers/DepartmentControllerTest.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.opencourse.configs.TestSecurityConfig;
 import org.opencourse.dto.request.DepartmentCreationDto;
 import org.opencourse.dto.request.DepartmentUpdateDto;
 import org.opencourse.models.Department;
@@ -31,6 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * @author Lee X ALEX
  */
 @WebMvcTest(DepartmentController.class)
+@Import(TestSecurityConfig.class)
 class DepartmentControllerTest {
 
     @Autowired
