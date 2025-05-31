3466ebde1b4fb69f05a381edcabd93234bfc3546
JingXiong <jingxionglee337@gmail.com>
2025-05-31 22:06:01 +0800

    feat: append TestSecurityConfig.java.

---
+++ src/test/java/org/opencourse/configs/TestSecurityConfig.java
@@ -0,0 +1,31 @@
+package org.opencourse.configs;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Test security configuration to disable security for unit tests.
+ * 
+ * @author Lee X ALEX
+ */
+@TestConfiguration
+@EnableWebSecurity
+public class TestSecurityConfig {
+
+    @Bean
+    @Primary
+    public SecurityFilterChain testSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
