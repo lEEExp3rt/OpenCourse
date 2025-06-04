96207ebe333a25261ee0f93aedb41df665dc446e
1EEExp3rt <1EEExpErt@zju.edu.cn>
2025-06-04 16:47:57 +0000

    fix: course

--- front_end/web/src/api/course.js
+++ front_end/web/src/api/course.js
@@ -5,7 +5,7 @@ const CourseApi = {
     return request.get('/course/all')
   },
   get_all_course_in_department(department_id){
-    return request.get('/course/department/',department_id)
+    return request.get('/course/department/' + department_id)
   },
   query(id) {
     return request.get(`/course/`, id);
